aed035c7dd0f616ba6d21f172e926e46f4546241
NEN10 <nen9ten10@gmail.com>
2020-05-07 08:26:00 +0530

    add random quotes

--- js/bg-color-change.js
+++ js/bg-color-change.js
@@ -1,19 +1,29 @@
 let body = document.querySelector("body")
-let button = document.querySelector("button")
+let button = document.querySelector(".button")
+let text = document.querySelector(".text")
+let quotation = document.querySelector(".quotation")
 
 body.style.backgroundColor="yellow";
-var node = document.createTextNode("This is new.");
 
-button.appendChild(node)
 
-button.addEventListener("mouseover",changeColor);
+button.addEventListener("click",changeColor);
 
 function changeColor() {
     let r = (Math.random()*10).toString().slice(-3)
     let g = (Math.random()*10).toString().slice(-3)
     let b = (Math.random()*10).toString().slice(-3)
+
+    const url = "https://api.quotable.io/random"
+    let Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET", url, false);
+    Httpreq.send(null);
+    
+    let responseData = JSON.parse(Httpreq.responseText);
+    
     
     let color="rgb("+r+","+g+", "+b+")"
+    quotation.textContent = responseData.content;
+    text.textContent=color;
     body.style.backgroundColor= color;
 
 }
